bc89e79257bf59c0bb5f726107f98e2158b34d9c
hmtstc-creator <hmtstc@gmail.com>
2026-04-03 15:12:12 +0300

    main

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
+let balance = 1000;
 
+function log(text){
+    let div = document.getElementById("logs");
+    div.innerHTML = text + "<br>" + div.innerHTML;
+}
+
+function buy(){
+    let symbol = document.getElementById("symbol").value;
+    let amount = document.getElementById("amount").value;
+
+    log("BUY " + symbol + " " + amount);
+}
+
+function sell(){
+    let symbol = document.getElementById("symbol").value;
+    let amount = document.getElementById("amount").value;
+
+    log("SELL " + symbol + " " + amount);
+}
